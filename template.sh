1968ca19339e8bac26ee03dc9a35af24669dd4ed
Varshith0810 <bomminenivarshith@gmail.com>
2025-09-14 21:10:34 +0000

    Add template.sh script

---
+++ template.sh
@@ -0,0 +1,14 @@
+#creating directtory
+mkdir -p src
+mkdir -p research
+ #creating files
+ touch src/__init__.py
+ touch src/helper.py
+ touch src/prompt.py
+ touch .env
+ touch setup.py
+ touch app.py
+ touch research/trails.ipynb
+ touch requirements.txt
+
+ echo "Directory and files created successfully"
